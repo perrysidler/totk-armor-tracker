9cc532b4146390457b234a1df6ccd721105b0700
perry-sidler <perry@perrysidler.com>
2023-05-28 10:02:31 -0700

    add missing data and sort, update favicon, add page title

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,12 +1,16 @@
 import "@/styles/globals.css";
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import Head from 'next/head';
 import { Fragment } from "react";
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
     return (
         <Fragment>
+            <Head>
+                <title>TOTK Armor Tracker</title>
+            </Head>
             <Component {...pageProps} />
         </Fragment>
     );
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -3,7 +3,11 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html className="h-full" lang="en">
-            <Head />
+            <Head>
+                <link rel="shortcut icon" href="/favicon.png" />
+                <link rel="apple-touch-icon" href="/favicon.png" />
+                <link rel="icon" href="/favicon.png" />
+            </Head>
             <body className="h-full">
                 <Main />
                 <NextScript />
